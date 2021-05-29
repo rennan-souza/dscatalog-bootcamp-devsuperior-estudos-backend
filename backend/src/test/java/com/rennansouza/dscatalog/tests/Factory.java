0e912d5cffdc6261fdc3596539b0cbcdaf515b64
Renan <souza.rnn@gmail.com>
2021-05-29 17:58:47 -0300

    Tests

---
+++ backend/src/test/java/com/rennansouza/dscatalog/tests/Factory.java
@@ -0,0 +1,25 @@
+package com.rennansouza.dscatalog.tests;
+
+import java.time.Instant;
+
+import com.rennansouza.dscatalog.dto.ProductDTO;
+import com.rennansouza.dscatalog.entities.Category;
+import com.rennansouza.dscatalog.entities.Product;
+
+public class Factory {
+
+	public static Product createProduct() {
+		Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2021-10-20T14:00:00Z"));
+		product.getCategories().add(createCategory());
+		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+	
+	public static Category createCategory() {
+		return new Category(1L, "Electronics");
+	}
+}
